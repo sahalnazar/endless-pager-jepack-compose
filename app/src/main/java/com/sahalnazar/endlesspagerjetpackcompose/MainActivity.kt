1ceb6d49a546139d03eb5eea0a6638a157cb0376
Sahalnazar <sahalnazar007@gmail.com>
2023-02-18 11:09:43 +0530

    Setup normal pager

--- app/src/main/java/com/sahalnazar/endlesspagerjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sahalnazar/endlesspagerjetpackcompose/MainActivity.kt
@@ -1,9 +1,24 @@
 package com.sahalnazar.endlesspagerjetpackcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.sahalnazar.endlesspagerjetpackcompose.ui.theme.EndlesspagerjepackcomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,8 +26,52 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             EndlesspagerjepackcomposeTheme {
-              Text(text = "Hello :)")  
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    EndlessHorizontalPager(
+                        items = listOf("A", "B", "C"),
+                        itemClicked = {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "item clicked: $it",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        })
+                }
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun EndlessHorizontalPager(
+    items: List<String>,
+    itemClicked: (String) -> Unit
+) {
+    val pagerState = rememberPagerState()
+
+    HorizontalPager(
+        modifier = Modifier
+            .background(Color(0xff469597))
+            .size(200.dp)
+            .clickable {
+                val clickedItem = items[pagerState.settledPage]
+                itemClicked(clickedItem)
+            },
+        pageCount = items.size,
+        state = pagerState
+    ) { page ->
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = items[page],
+                style = MaterialTheme.typography.h1
+            )
         }
     }
 }
